314812ee1d36a9bb20d88093cc2d35418bcfd73a
Yaël Balbastre <yael.balbastre@gmail.com>
2020-06-17 20:25:17 +0100

    Fixes

--- nntools/intensity_normalize.py
+++ nntools/intensity_normalize.py
@@ -1,7 +1,14 @@
 import numpy as np
 import nibabel as nb
 import os.path
-import copy
+
+# TODO:
+# . more tests
+# . other methods (percentiles...)
+# . option to not write on disk (if used as a library)
+# . commandline: how to deal with list of list of labels?
+# . make the code nicer
+
 
 class Normalizer:
     """Base class for normalizers."""
@@ -42,23 +49,26 @@ class Normalizer:
         return x, info
 
     def write(self, x, info):
-        output_ext = self.output_ext if self.output_ext is None \
-                     else info['ext'] if info['ext'] is not None \
+        output_ext = self.output_ext if self.output_ext \
+                     else info['ext'] if info['ext'] \
                      else 'nii.gz'
-        output_dir = self.output_dir if self.output_dir is None \
-                     else info['dir'] if info['dir'] is not None \
+        output_dir = self.output_dir if self.output_dir \
+                     else info['dir'] if info['dir']  \
                      else '.'
-        output_dtype = self.output_dtype if self.output_dtype is None \
-                       else info['dtype'] if info['dtype'] is not None \
+        output_dtype = self.output_dtype if self.output_dtype  \
+                       else info['dtype'] if info['dtype']  \
                        else x.dtype
-        output_fname = (self.output_prefix if self.output_prefix is not None
+        output_fname = (self.output_prefix if self.output_prefix
                         else '') + (info['fname'] if info['fname'] else '')
-        affine = info['class'].affine if info['class'] is not None else None
-        header = info['class'].header if info['class'] is not None else None
-        extra  = info['class'].extra if info['class'] is not None else None
+        affine = info['class'].affine if info['class'] else None
+        header = info['class'].header if info['class'] else None
+        extra = info['class'].extra if info['class'] else None
 
+        if len(x.shape) > 3 and x.shape[3] == 1:
+            x = x.reshape(x.shape[:3])
         nbobj = nb.spatialimages.SpatialImage(x, affine, header, extra)
         nbobj.header.set_data_dtype(output_dtype)
+        nbobj.header.set_data_shape(x.shape)
 
         output_path = os.path.join(output_dir, output_fname + output_ext)
         nb.save(nbobj, output_path)
@@ -66,9 +76,9 @@ class Normalizer:
 
     def normalize(self, x, *args, **kwargs):
         x, info = self.load(x)
-        x, *etc = self._normalize(x, *args, **kwargs)
+        x = self._normalize(x, *args, **kwargs)
         x = self.write(x, info)
-        return(x, *etc)
+        return x
 
     def _normalize(self, *args, **kwargs):
         raise NotImplementedError('Abstract method _normalize not implemented')
@@ -81,22 +91,20 @@ class ROINormalizer(Normalizer):
                  output_ext=None, output_dtype=None, output_dir=None,
                  output_prefix=None):
         super().__init__(output_ext, output_dtype, output_dir, output_prefix)
-        self.labels=labels
+        self.labels = labels
         self.metric = metric
         self.target = target
 
     def _normalize(self, x, labs):
         lab = self.load_label(labs)
-        print(x.shape)
-        print(lab.shape)
         if self.metric == 'mean':
             reference = np.average(x, weights=lab, dtype=np.float64)
         elif self.metric == 'median':
-            reference = np.average(x[lab>0.5])
+            reference = np.nanmedian(x[lab>0.5])
         else:
             raise TypeError("Metric must be 'mean' or 'median'. Got {}."
                             .format(self.metric))
-        x *= (self.target/reference)
+        x = x * (self.target/reference)
         return x
 
     def load_label(self, labs):
@@ -106,7 +114,6 @@ class ROINormalizer(Normalizer):
             labs = [labs]
         if len(labs) > 1:
             # Assume list of responsibilities
-            print('Responsibilities: multiple 3D') 
             if self.labels is not None:
                 labs = [labs[i] for i in self.labels]
             lab, _ = self.load(labs[0])
@@ -116,7 +123,6 @@ class ROINormalizer(Normalizer):
             lab, _ = self.load(labs[0])
             if len(lab.shape) > 3 and lab.shape[3] > 1:
                 # Assume 4D volume of responsibilities
-                print('Responsibilities: 4D')
                 if self.labels is not None:
                     lab = lab[:, :, :, self.labels]
                 lab = lab.sum(axis=3)
@@ -124,9 +130,9 @@ class ROINormalizer(Normalizer):
                 if self.labels is not None:
                     # Assume hard labels
                     lab = lab.astype(np.int)
-                    print(self.labels)
-                    print(np.unique(lab))
                     lab = np.isin(lab, self.labels)
+
+        lab = lab.reshape(lab.shape[:3])
         return lab
 
 # ----------------------------------------------------------------------
@@ -138,6 +144,12 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     from glob import glob
 
+    # This bit gives some flexibility for the input files:
+    # . unix-style ~ (HOME) is always expanded
+    # . unix-style tokens are always expanded
+    # . if an input file is a directories, we look for all image files
+    #   within. A list of supported format is specified below. In
+    #  theory, we could use all extensions supported by nibabel.
     possible_extensions = ['.nii', '.nii.gz', '.mgh', '.mgz']
     expansion_pattern = '*['
     for ext in possible_extensions[:-1]:
@@ -184,6 +196,9 @@ if __name__ == '__main__':
     }
     if output_args['output_dtype'] is not None:
         output_args['output_dtype'] = np.dtype(output_args['output_dtype'])
+    if output_args['output_ext'] and \
+            not output_args['output_ext'].startswith('.'):
+        output_args['output_ext'] = '.' + output_args['output_ext']
 
     if method == 'roi':
         obj = ROINormalizer(args.label_list, args.metric, args.target,
