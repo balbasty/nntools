4d318353a4029dcfe782f24a527a03240f8f15ca
Yaël Balbastre <yael.balbastre@gmail.com>
2020-07-21 12:42:32 +0100

    expose inspect

--- nntools/space/__init__.py
+++ nntools/space/__init__.py
@@ -3,6 +3,6 @@
 from .affine import affine_layout_matrix, affine_layout, change_layout, \
     affine_basis, affine_subbasis, affine_matrix, affine_parameters, \
     mean_affine, mean_space, voxel_size
-from .object import Reorienter
-from .functional import reorient
+from .object import Reorienter, Inspecter
+from .functional import reorient, inspect
 
